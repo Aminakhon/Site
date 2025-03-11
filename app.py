28224504a5e6dca763206f99be95ed8b355b538b
Аминахон Тухтаева <aminakhon09@mail.ru>
2025-03-11 12:03:56 +0300

    2

--- app.py
+++ app.py
@@ -10,20 +10,7 @@ db = SQLAlchemy(app)
 Migrate(app, db)
 
 # модель для хранения задачи
-class Bads(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.String(1000), nullable=False, default=False)
-    time = db.Column(db.String(80), nullable=False)
 
-class Users(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    password = db.Column(db.String(100), nullable=False, default=False)
-    phone = db.Column(db.String(100), nullable=False, default=False)
-    email = db.Column(db.String(100), nullable=False, default=False)
-
-class User_Bad(db.Model):
 
 
 # презентер для задачи
