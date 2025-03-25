4b5d9c32d6bfa127024e2534589100f0b86ef318
Аминахон Тухтаева <aminakhon09@mail.ru>
2025-03-25 11:56:42 +0300

    Merge remote-tracking branch 'origin/main'

--- app.py
+++ app.py
@@ -3,6 +3,12 @@ from models import *
 from presents import *
 
 
+def presentsup(id):
+    supplements = db.session.query(Supplements)\
+                  .join(User_Supplements)\
+                  .filter(User_Supplements.user_id == id)\
+                  .all()
+    return [present_supplement(sup) for sup in supplements]
 
 
 @app.route('/logout')
@@ -25,11 +31,11 @@ def add_login():
     password = request.form.get('password')
     user = Users.query.filter_by(login=login).first()
 
-    if user and user.password == password:
-        return redirect('/api/<user.id>/profile')
-
+    if user and bcrypt.check_password_hash(user.password, password):
+        session['user_id'] = user.id  # Сохраняем ID пользователя в сессии
+        return redirect(f'/api/{user.id}/timetable')
     else:
-        return render_template('login.html', error='Что-то не так')
+        return render_template('login.html', error='Неверные учетные данные')
 
 @app.route('/registred', methods=['GET'])
 def reg():
@@ -41,30 +47,34 @@ def regin():
     login = request.form.get('login')
     password = request.form.get('password')
     name = request.form.get('name')
-    user = Users.query.filter_by(login=login).first()
-    if user:
-        return render_template('reg.html', error='Пользователь с данным логином уже зарегистрирован')
-    elif login and password and name:
 
-        user = Users(login=login, name=name, password=password)
+    if not all([login, password, name]):
+        return render_template('reg.html', error='Все поля обязательны для заполнения')
 
-        db.session.add(user)
-        db.session.commit()
-        return redirect('/api/<user.id>/profile')
-    else:
-        return render_template('reg.html', error='Вы не можете иметь пустой логин/пароль/имя')
+    if Users.query.filter_by(login=login).first():
+        return render_template('reg.html', error='Пользователь с таким логином уже существует')
 
-@app.route('/api/<int:id>/profile')
+    hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+    user = Users(login=login, name=name, password=hashed_password)
+
+    db.session.add(user)
+    db.session.commit()
+
+    session['user_id'] = user.id  # Сохраняем ID пользователя в сессии
+    return redirect(f'/api/{user.id}/timetable')
+
+@app.route('/api/<int:id>/timetable')
 def prof(id):
     user = Users.query.filter_by(id=id).first()
     present_user(user)
-    return render_template('reg.html', error = '', user=user)
+    notes = presentsup(id)
+    return render_template('timetable.html', error = '', user=user, notes=notes, user_id=id)
 
 # получаем все задачи
 @app.route('/api/<int:id>/library', methods=['GET'])
 def get_supplements(id):
     supplements = Supplements.query.all()
-    return render_template('library.html', supplements=supplements, id=id)
+    return render_template('library.html', supplements=supplements, user_id=id)
 
 @app.route('/api/<int:user_id>/library/<int:sup_id>', methods=['POST'])
 def add_supplements(user_id, sup_id):
@@ -73,7 +83,7 @@ def add_supplements(user_id, sup_id):
     user = Users.query.filter_by(user_id=user_id, supplement_id=sup_id).first()
 
     if user:
-        return render_template('library.html', error='БАД уже добавлен')
+        return render_template('library.html', error='БАД уже добавлен', user_id=user_id)
 
     user_supplement = User_Supplements(user_id=user_id, supplement_id=sup_id)
 
@@ -82,10 +92,10 @@ def add_supplements(user_id, sup_id):
 
     return redirect('/api/<user_id>/timetable')
 
-@app.route('/api/<int:id>/timetable', methods=['GET'])
+@app.route('/api/<int:id>/profile', methods=['GET'])
 def get_timetable(id):
     supplements = User_Supplements.query.filter_by(user_id=id)
-    return render_template('timetable.html', supplements=supplements)
+    return render_template('profile.html', supplements=supplements, user_id=id)
 
 
 
--- models.py
+++ models.py
@@ -9,15 +9,16 @@ class Supplements(db.Model):
 
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.String(100), unique=True, nullable=False)  # Добавьте это поле
     name = db.Column(db.String(100), nullable=False)
     password = db.Column(db.String(100), nullable=False, default=False)
     phone = db.Column(db.String(100), nullable=False, default=False)
     email = db.Column(db.String(100), nullable=False, default=False)
 
 class User_Supplements(db.Model):
-    id_ = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
-    supplement_id = db.Column(db.Integer, db.ForeignKey('supplement.id', ondelete='CASCADE'), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
+    supplement_id = db.Column(db.Integer, db.ForeignKey('supplements.id', ondelete='CASCADE'), nullable=False)
 
-    user = db.relationship('Users', backref=db.backref('site', lazy=False))
-    supplement = db.relationship('Supplements', backref=db.backref('site', lazy=True), lazy=False)
+    user = db.relationship('Users', backref=db.backref('user_supplements', lazy=True))
+    supplement = db.relationship('Supplements', backref=db.backref('user_supplements', lazy=True))
