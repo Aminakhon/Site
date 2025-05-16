87fd720abc186cf13c75ff35d5b4151ae2dd1f43
Аминахон Тухтаева <aminakhon09@mail.ru>
2025-05-16 14:19:19 +0300

    Merge remote-tracking branch 'origin/main'

--- setup.sh
+++ setup.sh
@@ -1,2 +1,3 @@
-cp flask_app.nginx.conf /etc/nginx/sites-available/sup_app.nginx.conf
-certbot --nginx -d silaeder.codingprojects.ru
+cp sup_app.nginx.conf /etc/nginx/sites-available/sup_app.nginx.conf
+cp template.db ./instance/site.db
+certbot --nginx -d sup.silaeder.codingprojects.ru
